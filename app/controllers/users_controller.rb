e9bba0d06732fb8dd3c31ed273acfa5e84ffb0bc
HirakawaYuuki <yukiryan1@gmail.com>
2019-06-22 14:18:06 +0900

    再更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!,only:[:new, :create, :index, :show, :edit, :update, :destroy]
-    before_action :current_user,only:[:edit,:update,:destroy]
+    
 
   def show
   	@user = User.find(params[:id])
@@ -66,17 +66,13 @@ end
 
   private
 
-def user_params
+    def user_params
     params.require(:user).permit(:name, :profile_image, :introduction_id )
 	
-end
- def correct_user
+    end
+ 
 
-	if @user != current_user
- 		redirect_to user_path(current_user)
-		
- 	end
- end
+
     
 
-end
+  end
